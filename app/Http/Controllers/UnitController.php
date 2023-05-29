b7e4defc0fd7d94d777acfe8d31a7b96901db29c
Quang Dat Phan <90644699+davephan09@users.noreply.github.com>
2023-05-29 14:52:52 +0700

    fix comment

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -18,8 +18,6 @@ class UnitController extends Controller
 
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
