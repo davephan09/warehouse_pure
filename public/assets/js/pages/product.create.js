11d9216a7ecdafa61b0e6f03bae9d3237788837d
Quang Dat Phan <90644699+davephan09@users.noreply.github.com>
2023-06-07 18:04:21 +0700

    fix delete tax field in create product

--- public/assets/js/pages/product.create.js
+++ public/assets/js/pages/product.create.js
@@ -58,6 +58,15 @@ var ProductCreateClass = function() {
             $clone.find('.select2-container').each((i, el) => {
                 $(el).remove();
             });
+            var btnRemove = `<button type="button" data-repeater-delete="" class="btn btn-sm btn-icon btn-light-danger btn-remove-tax">
+                    <span class="svg-icon svg-icon-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <rect opacity="0.5" x="7.05025" y="15.5356" width="12" height="2" rx="1" transform="rotate(-45 7.05025 15.5356)" fill="currentColor" />
+                            <rect x="8.46447" y="7.05029" width="12" height="2" rx="1" transform="rotate(45 8.46447 7.05029)" fill="currentColor" />
+                        </svg>
+                    </span>
+                </button>`
+            $clone.append(btnRemove)
             $clone.hide().appendTo(ele.listAddTax).promise().done(function() {
                 $(this).slideDown()
             })
