e6d69e4a33d3ae242bf2cea70f422f1403fd2723
Quang Dat Phan <90644699+davephan09@users.noreply.github.com>
2023-06-10 22:29:52 +0700

    update create product view

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -315,54 +315,11 @@
             <!--begin::Tab pane-->
             <div class="tab-pane fade" id="kt_ecommerce_add_product_advanced" role="tab-panel">
                 <div class="d-flex flex-column gap-7 gap-lg-10">
-                    <!--begin::Inventory-->
                     <div class="card card-flush py-4">
                         <!--begin::Card header-->
                         <div class="card-header">
                             <div class="card-title">
                                 <h2>{{__('common.inventory')}}</h2>
-                            </div>
-                        </div>
-                        <!--end::Card header-->
-                        <!--begin::Card body-->
-                        <div class="card-body pt-0">
-                            <!--begin::Input group-->
-                            <div class="mb-10 fv-row">
-                                <!--begin::Label-->
-                                <label class="required form-label">SKU - {{__('product.code')}}</label>
-                                <!--end::Label-->
-                                <!--begin::Input-->
-                                <input type="text" name="sku" class="form-control mb-2" placeholder="{{__('product.code_example')}}" value="" />
-                                <!--end::Input-->
-                                <!--begin::Description-->
-                                <div class="text-muted fs-7">{{__('product.code_des')}}</div>
-                                <!--end::Description-->
-                            </div>
-                            <!--end::Input group-->
-                            <!--begin::Input group-->
-                            <div class="mb-10 fv-row">
-                                <!--begin::Label-->
-                                <label class="required form-label">{{__('common.quantity')}}</label>
-                                <!--end::Label-->
-                                <!--begin::Input-->
-                                <div class="d-flex gap-3">
-                                    <input type="number" name="warehouse" class="form-control mb-2" placeholder="{{__('common.in_warehouse')}}" />
-                                </div>
-                                <!--end::Input-->
-                                <!--begin::Description-->
-                                <div class="text-muted fs-7">{{__('product.quantity_des')}}</div>
-                                <!--end::Description-->
-                            </div>
-                            <!--end::Input group-->
-                        </div>
-                        <!--end::Card header-->
-                    </div>
-                    <!--end::Inventory-->
-                    <div class="card card-flush py-4">
-                        <!--begin::Card header-->
-                        <div class="card-header">
-                            <div class="card-title">
-                                <h2>{{__('common.variation')}}</h2>
                             </div>
                             <div class="">
                                 <label class="form-check form-switch form-check-custom form-check-solid">
@@ -438,18 +395,6 @@
                                                     </tr>
                                                 </thead>
                                                 <tbody id="body-detail-variations">
-                                                    {{-- <tr>
-                                                        <td><input type="text" class="form-control mw-100" name="" disabled value="White-S" /></td>
-                                                        <td><input type="number" class="form-control mw-100" name="" value="" /></td>
-                                                        <td><input type="number" class="form-control mw-100" name="" value="" /></td>
-                                                        <td><input type="text" class="form-control mw-100" name="" value="Tét" /></td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td><input type="text" class="form-control mw-100" name="" disabled value="White-M" /></td>
-                                                        <td><input type="number" class="form-control mw-100" name="" value="" /></td>
-                                                        <td><input type="number" class="form-control mw-100" name="" value="" /></td>
-                                                        <td><input type="text" class="form-control mw-100" name="" value="Tét" /></td> --}}
-                                                    </tr>
                                                 </tbody>
                                             </table>
                                         </div>
