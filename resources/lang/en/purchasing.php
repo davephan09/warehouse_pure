ddd1f7cfbe0f0bcabecabe597faa104e7df70769
Quang Dat Phan <90644699+davephan09@users.noreply.github.com>
2023-06-20 22:26:40 +0700

    update lang purchasing

---
+++ resources/lang/en/purchasing.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'create' => 'Add New Purchasing',
+    'purchasing_details' => 'Purchasing Details',
+    'supplier' => 'Supplier',
+    'select_supplier' => 'Select a supplier',
+    'supplier_des' => 'The supplier who provides this order.',
+    'date' => 'Purchasing date',
+    'select_date' => 'Select date',
+    'date_des' => 'Set the date of the order to process.',
+    'select_product' => 'Select product',
+];
---
+++ resources/lang/vi/purchasing.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'create' => 'Nhập hàng',
+    'purchasing_details' => 'Chi tiết Nhập hàng',
+    'supplier' => 'Nhà cung cấp',
+    'select_supplier' => 'Chọn nhà cung cấp',
+    'supplier_des' => 'Nhà cung cấp nhập đơn hàng này.',
+    'date' => 'Ngày nhập hàng',
+    'select_date' => 'Lựa chọn ngày',
+    'date_des' => 'Chọn ngày nhập đơn hàng này.',
+    'select_product' => 'Chọn sản phẩm',
+];
