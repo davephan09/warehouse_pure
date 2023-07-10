812ba427db3bb7c7704bb72cc2d591ffe5db9acd
Quang Dat Phan <90644699+davephan09@users.noreply.github.com>
2023-07-10 23:36:03 +0700

    add num row enums

---
+++ app/Enums/NumRowPage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Contracts\LocalizedEnum;
+use BenSampo\Enum\Enum;
+
+final class NumRowPage extends Enum implements LocalizedEnum
+{
+    const Ten           =   10;
+    const Fifteen       =   15;
+    const Twelve        =   20;
+    const ThirtyFive    =   35;
+    const Fifty         =   50;
+    const Hundred       =   100;
+    const TwoHundred    =   200;
+
+}
--- resources/lang/en/enums.php
+++ resources/lang/en/enums.php
@@ -1,11 +1,22 @@
 <?php
 
 use App\Enums\ActiveStatus;
+use App\Enums\NumRowPage;
 
 return [
     ActiveStatus::class => [
         ActiveStatus::All => 'All',
         ActiveStatus::Active => 'Active',
         ActiveStatus::Inactive => 'Inactive',
+    ],
+
+    NumRowPage::class => [
+        NumRowPage::Ten => 10,
+        NumRowPage::Fifteen => 15,
+        NumRowPage::Twelve => 20,
+        NumRowPage::ThirtyFive => 35,
+        NumRowPage::Fifty => 50,
+        NumRowPage::Hundred => 100,
+        NumRowPage::TwoHundred => 200,
     ],
 ];
