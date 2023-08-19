df294711c1cccd618db675c1974e584d8272573d
Quang Dat Phan <90644699+davephan09@users.noreply.github.com>
2023-08-19 22:06:38 +0700

    update authenticate middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 
 class Authenticate extends Middleware
 {
@@ -17,5 +20,27 @@ class Authenticate extends Middleware
         if (! $request->expectsJson()) {
             return route('login');
         }
+    }
+
+    public function handle($request, Closure $next, ...$guard)
+    {
+        $userInfo = Auth::user();
+        if (empty($userInfo)) {
+            $curUrl = url()->full();
+            $curUrl = urlencode($curUrl);
+
+            if ($request->ajax()) {
+                $data = [
+                    'error' => true,
+                    'message' => trans('auth.Unauthorized')
+                ];
+                return response($data, 401)->header('REQUIRES_AUTH', '1');
+            } else {
+                return redirect(url('/login?_b=' . $curUrl));
+            }
+        }
+
+        View::share('userInfo', $userInfo);
+        return $next($request);
     }
 }
